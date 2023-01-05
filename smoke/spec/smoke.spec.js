b0a1e034a225659a2f5888ca724b4d9935c9f324
torloadna <adna.torlo@edu.fit.ba>
2023-01-05 19:17:10 +0100

    smoke test last version

--- smoke/spec/smoke.spec.js
+++ smoke/spec/smoke.spec.js
@@ -1,32 +1,37 @@
 const homepage = require('../../regression/pageObjects/homepage');
-const productCategories = require('../../regression/pageObjects/productCategories');
-const item = require('../../regression/pageObjects/item')
+const item = require('../../regression/pageObjects/item');
 const login = require('../../regression/pageObjects/login');
+const productCategories = require('../../regression/pageObjects/productCategories');
 
 describe('smoke test',()=>{
     beforeAll(async function() {  
         browser.ignoreSynchronization = true;
         await browser.get('http://ec2-3-73-53-128.eu-central-1.compute.amazonaws.com:8090/');
         await browser.manage().window().maximize();
-    });
+    })
 
     it('smoke',async ()=>{
         await homepage.clickOn("Login");
-        await login.insertEmail("adna.torlo@edu.fit.ba");
+        await login.insertEmail("adnatorlo@edu.cake");
         await login.insertPassword("adnatorlo123!");
         await login.rememberMe();
         await login.clickLogin();
         await homepage.clickOn("SHOP");
-        await homepage.clickOnById("explore-more");
-        await homepage.clickOnIcon("//h1[normalize-space()='New Balance Shoe']");
-        await item.insertBid("64");
+        await productCategories.clickOn("explore-more");
+        await browser.sleep(2000);
+        await productCategories.clickOnX("//h1[normalize-space()='New Balance Shoe']");
+        await browser.sleep(3000);
+        await item.insertBid("101");
         await item.placeBid();
-        await expect(element.all(by.id("regular-span")).isPresent()).toBe(true);
-        await homepage.clickOn("SHOP");
-        await homepage.clickOnIcon("//h1[normalize-space()='Sculpture']");
-        await item.insertBid("123");
+        await browser.sleep(2000);
+        await expect(element.all(by.id("message-container-invalid")).isPresent()).toBe(true);
+        await item.clickOn("SHOP");
+        await browser.sleep(2000);
+        await productCategories.clickOnX("//h1[normalize-space()='Swimming Shorts']");
+        await browser.sleep(2000);
+        await item.insertBid("49");
         await item.placeBid();
         await browser.sleep(3000);
-        await expect(element.all(by.id("bold-span")).getText()).toContain("Invalid!")
+        await expect(element.all(by.id("message-container-topbid")).isPresent()).toBe(true);
     })
 })
